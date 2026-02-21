4867dbcf6f893e6ee2b3ce9604714476ef26dd5e
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2026-02-21 07:14:32 -0500

    add login info to get_info

--- bin/get_info.py
+++ bin/get_info.py
@@ -51,6 +51,7 @@ async def main():
     dayTime = time.strftime("%Y-%m-%d")
 
     functions = {
+        "info": None,
         "api-energy/electric/getFhpElectricData": {"type": 1, "dayTime": dayTime},
         "hes-gateway/common/getAccessoryList": None,
         "hes-gateway/common/getPageByTypeList": {
@@ -69,7 +70,6 @@ async def main():
         "hes-gateway/terminal/getDeviceCompositeInfo": {"refreshFlag": 0},
         "hes-gateway/terminal/getDeviceInfoV2": None,
         "hes-gateway/terminal/getGatewaySystemSetting": None,
-        "hes-gateway/terminal/getHomeGatewayList": None,
         "hes-gateway/terminal/getHotSpotInfo/v2": None,
         "hes-gateway/terminal/getPersonalInfo": None,
         "hes-gateway/terminal/getShowTip": {"type": 1},
@@ -116,7 +116,9 @@ async def main():
         "get_stats": None,
     }
 
-    async def get(func: str) -> None:
+    async def get(func: str):
+        if func == "info":
+            return fetcher.info
         if func.startswith(("api-energy", "hes-gateway")):
             return (await client._get(client.url_base + func, functions[func]))[  # noqa: SLF001
                 "result"
