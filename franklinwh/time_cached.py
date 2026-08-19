d11d2fc09bf345d5769b3ec5e8cc30cc8f6ceceb
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2026-08-19 07:56:01 -0400

    prefer TTLCache to Roll Your Own

--- franklinwh/time_cached.py
+++ franklinwh/time_cached.py
@@ -6,28 +6,28 @@ cache arguments and results indefinitely, think 'self' for member functions.
 """
 
 import asyncio
-from datetime import datetime, timedelta
+from datetime import timedelta
 from functools import wraps
+
+from cachetools import TTLCache
 
 
 def time_cached(ttl: timedelta = timedelta(seconds=2)):
     """Decorator to cache function results for a specified time-to-live (TTL)."""
 
     def wrapper(func):
-        __cache = {}
+        __cache = TTLCache(maxsize=10, ttl=ttl.seconds)
         __lock = asyncio.Lock()
 
         @wraps(func)
         async def wrapped(*args, **kwargs):
             async with __lock:
-                now = datetime.now()  # pylint: disable=home-assistant-enforce-naive-now
-                for key, value in __cache.copy().items():
-                    if now > value[0]:
-                        del __cache[key]
                 key = (args, frozenset(kwargs.items()))
-                if key not in __cache:
-                    __cache[key] = (now + ttl, await func(*args, **kwargs))
-                return __cache[key][1]
+                if key in __cache:
+                    return __cache[key]
+                value = await func(*args, **kwargs)
+                __cache[key] = value
+                return value
 
         wrapped.clear = __cache.clear
         return wrapped
